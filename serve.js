36ab181b26ead9903b81302483470370b8992b5d
Jack Crane <jack@jackcrane.rocks>
2024-06-21 12:14:06 -0400

    Add env logging

--- serve.js
+++ serve.js
@@ -8,6 +8,8 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+console.log(process.env);
+
 main();
 
 const app = express();
